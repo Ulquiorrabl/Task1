14c1588e2dc691315890c4d6a5ca853932246c77
Vladislav <ulqrbl@gmail.com>
2020-12-11 13:45:09 +0300

    namespace fix
    fixed relocated namestace references

--- CarPark/ParkImplementation/Park.cs
+++ CarPark/ParkImplementation/Park.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task1.Cars;
-using Task1.States;
+using Task1.States.ParkStates;
 
 namespace Task1.CarPark.ParkImplementation
 {
